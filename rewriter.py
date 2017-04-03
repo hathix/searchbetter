799d596c3b99392dceefe914672037306de619ef
Neel Mehta <neelmehta@college.harvard.edu>
2017-04-03 16:35:43 -0400

    create class for query rewriting search engines

--- rewriter.py
+++ rewriter.py
@@ -17,6 +17,13 @@ class Rewriter(object):
         raise NotImplementedError("Subclasses must implement!")
 
 
+class ControlRewriter(Rewriter):
+    """
+    A rewriter that's basically a no-op. Just returns the term you give it.
+    """
+
+    def rewrite(self, term):
+        return [term]
 
 
 
@@ -39,7 +46,10 @@ class WikipediaRewriter(Rewriter):
         tree = etree.fromstring(r.text)
         # TODO: ignore wikipedia internal categories
         # https://en.wikipedia.org/wiki/Category:Tracking_categories
-        return [self.clean_category(x.get('title')) for x in tree.findall('.//cl')]
+        wikipedia_results = [self.clean_category(x.get('title')) for x in tree.findall('.//cl')]
+
+        # append the original term just for completeness
+        return wikipedia_results + [term]
 
 
 
@@ -57,5 +67,23 @@ class RewritingSearchEngine(object):
         self.search_engine = search_engine
 
 
+    def flatten(self, l):
+        """
+        Flattens a list.
+        """
+        return [item for sublist in l for item in sublist]
+
+
     def search(self, term):
-        pass
+        """
+        Using the given search term (or query), rewrites it according to the
+        rewriter and then passes that through the search engine.
+        """
+        rewritten_queries = self.rewriter.rewrite(term)
+
+        results = [self.search_engine.search(q) for q in rewritten_queries]
+
+        # results are multi-level... flatten it
+        flattened_results = self.flatten(results)
+
+        return flattened_results
