e59c768ac922cb50e12a8c45a8c8d70e20b15aa1
Neel Mehta <neelmehta@college.harvard.edu>
2017-04-13 18:09:46 -0400

    describing my issue

--- corpus.py
+++ corpus.py
@@ -1,13 +1,19 @@
-import gensim.models as models
-import gensim.models.word2vec as word2vec
-
-"""
-Utilities for generating a corpus.
-"""
-
-def generate_bigram_corpus(raw_corpus):
-    """
-    Given a raw corpus (like the Wikipedia corpus or a custom corpus made of
-    words on each line), returns a new corpus that recognizes bigrams. You can
-    drop this into word2vec instead of the raw corpus.
-    """
+# import gensim.models as models
+# import gensim.models.word2vec as word2vec
+# import gensim.models.phrases.Phrases as Phrases
+# import gensim.models.phrases.Phraser as Phraser
+#
+# """
+# Utilities for generating a corpus.
+# """
+#
+# def generate_bigram_corpus(raw_corpus):
+#     """
+#     Given a raw corpus (like the Wikipedia corpus or a custom corpus made of
+#     words on each line), returns a new corpus that recognizes bigrams. You can
+#     drop this into word2vec instead of the raw corpus.
+#     """
+#     # Phrases object generates some bigrams
+#     bigram_phrases = Phrases(raw_corpus)
+#     # and the Phraser object compresses and improves this
+#     bigram_phraser = Phraser(bigram_phrases)
