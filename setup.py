24d3ef0d25dde7e28f8b21be9d61068e916f3046
Neel Mehta <neelmehta@college.harvard.edu>
2017-04-25 18:00:06 -0400

    add setup for pypi'

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'searchbetter',
+  packages = ['searchbetter'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Query rewriting for search engines on small corpuses ',
+  author = 'Neel Mehta',
+  author_email = 'neelmehta96@gmail.com',
+  url = 'https://github.com/hathix/searchbetter', # use the URL to the github repo
+  download_url = 'https://github.com/hathix/searchbetter/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['search engine', 'query rewriting', 'search', 'word2vec', 'nlp'], # arbitrary keywords
+  classifiers = [],
+)
