557019e531c977da862cf4625693c27d82ea3031
Neel Mehta <neelmehta@college.harvard.edu>
2017-05-03 14:34:55 -0400

    count num words

--- searchbetter/utils.py
+++ searchbetter/utils.py
@@ -19,3 +19,20 @@ def unique_words_in_string(string):
     unique_words = set(words)
 
     return len(unique_words)
+
+
+def unique_words_of_field(engine, field_name):
+    """
+    Approximates the set of unique words stored in a single field (column)
+    of a SearchEngine.
+    """
+    # get all unique elements in text from the search engine
+    entries = engine.index.searcher().lexicon(field_name)
+
+    # only include words
+    texts = [w for w in entries if re.match(r"\b[a-zA-Z_']+\b", w) is not None]
+
+    # lowercase and unique it all
+    lowers = [w.lower() for w in texts]
+
+    return set(lowers)
