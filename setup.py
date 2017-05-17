8e1c19f8bc9c8b151191085347e8c690e0690d2e
Neel Mehta <neelmehta@college.harvard.edu>
2017-05-17 13:43:45 -0400

    improve setup

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+
+version = '0.4'
+
 setup(
   name = 'searchbetter',
   packages = ['searchbetter'], # this must be the same as the name above
-  version = '0.3',
+  version = version,
   description = 'Query rewriting for search engines on small corpuses',
   author = 'Neel Mehta',
   author_email = 'neelmehta96@gmail.com',
   url = 'https://github.com/hathix/searchbetter', # use the URL to the github repo
-  download_url = 'https://github.com/hathix/searchbetter/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/hathix/searchbetter/archive/{}.tar.gz'.format(version),
   keywords = ['search engine', 'query rewriting', 'search', 'word2vec', 'nlp'], # arbitrary keywords
   classifiers = [],
 )
